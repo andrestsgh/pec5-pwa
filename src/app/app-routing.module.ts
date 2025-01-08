4ffc3a346a962709d3257d921c99b511fc4856bd
Andrés Terol Sánchez <andrests78@gmail.com>
2025-01-08 22:23:08 +0100

    DFA - PEC5.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './components/list/list.component';
+import { DetailComponent } from './components/detail/detail.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'character/:id', component: DetailComponent },
+  { path: '**', component: ListComponent },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
